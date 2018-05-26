0f347bd0b77c5224564f16d375b6fc2bedf38c8b
Homero Barbosa <hbarbosa@paylocity.com>
2018-05-26 15:14:35 -0700

    Add sql project

---
+++ src/BugHub.Sql/Tables/Bugs.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[Bugs]
+(
+  [Id] BIGINT NOT NULL PRIMARY KEY IDENTITY (1,1),
+  [Title] NVARCHAR(1024) NOT NULL,
+  [Description] NVARCHAR(MAX) NOT NULL,
+  [CreationDate] DATETIME2 NOT NULL,
+  [LastModificationDate] DATETIME2 NOT NULL
+)
+GO;
